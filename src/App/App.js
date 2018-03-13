611e1068bae81debfb54a02f0a6546cf3d0069b4
Richard Afro <richyafro@hotmail.com>
2018-03-13 22:49:14 +0100

    Add context provider

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Provider from '../Provider/Provider';
 import Main from '../Home/Main';
 import './App.css';
 
 const App = () => (
   <Router>
-    <div>
-      <Route path="/" component={Main} />
-    </div>
+    <Provider>
+      <div>
+        <Route path="/" component={Main} />
+      </div>
+    </Provider>
   </Router>
 );
 
