5e2b114676553ceb2c474d0ee06ce7b164612a34
Richard Afro <richyafro@hotmail.com>
2018-03-13 22:51:19 +0100

    Expose paginator to app state

--- src/Paginator/Paginator.js
+++ src/Paginator/Paginator.js
@@ -2,40 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ChevronLeft, ChevronRight } from 'react-feather';
 
+import { AppContext } from '../Provider/Provider';
 import { COLOR_CODE } from '../helpers/helper';
 
 import './Paginator.css';
 
-const Paginator = ({ direction, clickHandler, disabled }) => (
+const Paginator = ({ direction }) => (
   <div className="paginator">
-    {direction === 'left' ? (
-      <button
-        onClick={e => clickHandler(e, 'PREV')}
-        className="pagination-button"
-        disabled={!disabled}
-      >
-        <ChevronLeft className="prev" color={disabled ? `${COLOR_CODE}` : 'grey'} size={40} />
-      </button>
-    ) : (
-      <button
-        onClick={e => clickHandler(e, 'NEXT')}
-        className="pagination-button"
-        disabled={!disabled}
-      >
-        <ChevronRight className="next" color={disabled ? `${COLOR_CODE}` : 'grey'} size={40} />
-      </button>
-    )}
+    <AppContext.Consumer>
+      {({ callbacks, state }) => (
+        <React.Fragment>
+          {direction === 'left' ? (
+            <button
+              onClick={e => callbacks.handlePagination(e, 'PREV')}
+              className="pagination-button"
+              disabled={!state.hasPrev}
+            >
+              <ChevronLeft
+                className="prev"
+                color={state.hasPrev ? `${COLOR_CODE}` : 'grey'}
+                size={40}
+              />
+            </button>
+          ) : (
+            <button
+              onClick={e => callbacks.handlePagination(e, 'NEXT')}
+              className="pagination-button"
+              disabled={!state.hasNext}
+            >
+              <ChevronRight
+                className="next"
+                color={state.hasNext ? `${COLOR_CODE}` : 'grey'}
+                size={40}
+              />
+            </button>
+          )}
+        </React.Fragment>
+      )}
+    </AppContext.Consumer>
   </div>
 );
 
 Paginator.propTypes = {
-  clickHandler: PropTypes.func.isRequired,
   direction: PropTypes.string.isRequired,
-  disabled: PropTypes.bool,
-};
-
-Paginator.defaultProps = {
-  disabled: true,
 };
 
 export default Paginator;
