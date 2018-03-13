ffed761f1a9173d6ae35d4a64329833cc441749d
Richard Afro <richyafro@hotmail.com>
2018-03-13 22:55:50 +0100

    Uodate sidebar to use context state

--- src/Sidebar/SideBar.js
+++ src/Sidebar/SideBar.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import SideBarForm from './SideBarForm/SideBarForm';
 
 import './SideBar.css';
 
-const SideBar = props => (
+const SideBar = () => (
   <div className="gh-sidebar">
     <h3 className="filter-header">Filters</h3>
-    <SideBarForm {...props} />
+    <SideBarForm />
   </div>
 );
-
-SideBar.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  language: PropTypes.string.isRequired,
-  repoCount: PropTypes.objectOf(PropTypes.any).isRequired,
-  followerCount: PropTypes.objectOf(PropTypes.any).isRequired,
-};
 
 export default SideBar;
--- src/Sidebar/SideBarForm/SideBarForm.js
+++ src/Sidebar/SideBarForm/SideBarForm.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 import Select from 'react-select';
 import Switch from 'react-toggle-switch';
 
 import 'react-select/dist/react-select.css';
 import 'react-toggle-switch/dist/css/switch.min.css';
 
+import { AppContext } from '../../Provider/Provider';
 import { getCountries, filterData } from '../../helpers/helper';
 import FormControl from './FormControl';
-
 
 class SideBarForm extends React.Component {
   constructor(props) {
@@ -26,89 +24,75 @@ class SideBarForm extends React.Component {
   }
 
   render() {
-    const {
-      language,
-      onChange,
-      limit,
-      followerCount,
-      repoCount,
-      country,
-    } = this.props;
-
     const { languages, limitList } = filterData;
 
     const fllwrLabel = {
-      bool: this.props.followerCount.greaterThan,
       first: 'Greater than 1000',
       second: 'Less than 1000',
     };
     const repoLabel = {
-      bool: this.props.repoCount.greaterThan,
       first: 'Greater than 50',
       second: 'Less than 50',
     };
 
     return (
-      <form>
-        <FormControl header="Language Options">
-          <Select
-            name="language-filter"
-            value={language}
-            onChange={e => onChange(e, 'SET_LANGUAGE')}
-            options={languages}
-            className="form-select"
-          />
-        </FormControl>
+      <AppContext.Consumer>
+        {({ callbacks, state }) => (
+          <form>
+            <FormControl header="Language Options">
+              <Select
+                name="language-filter"
+                value={state.language}
+                onChange={e => callbacks.filterChangeHandler(e, 'SET_LANGUAGE')}
+                options={languages}
+                className="form-select"
+              />
+            </FormControl>
 
-        <FormControl header="Result Limit">
-          <Select
-            name="result limit"
-            value={limit}
-            onChange={e => onChange(e, 'SET_LIMIT')}
-            options={limitList}
-            className="form-select"
-          />
-        </FormControl>
+            <FormControl header="Result Limit">
+              <Select
+                name="result limit"
+                value={state.limit}
+                onChange={e => callbacks.filterChangeHandler(e, 'SET_LIMIT')}
+                options={limitList}
+                className="form-select"
+              />
+            </FormControl>
 
-        <FormControl header="Location">
-          <Select
-            name="countries"
-            value={country}
-            onChange={e => onChange(e, 'SET_COUNTRY')}
-            options={this.state.countries}
-            className="form-select"
-          />
-        </FormControl>
+            <FormControl header="Location">
+              <Select
+                name="countries"
+                value={state.country}
+                onChange={e => callbacks.filterChangeHandler(e, 'SET_COUNTRY')}
+                options={this.state.countries}
+                className="form-select"
+              />
+            </FormControl>
 
-        <FormControl header="Followers Count" label={fllwrLabel}>
-          <Switch
-            onClick={e => onChange(e, 'SET_FOLLOWER_COUNT')}
-            on={followerCount.greaterThan}
-          />
-        </FormControl>
+            <FormControl
+              header="Followers Count"
+              label={{ ...fllwrLabel, bool: state.followerCount.greaterThan }}
+            >
+              <Switch
+                onClick={e => callbacks.filterChangeHandler(e, 'SET_FOLLOWER_COUNT')}
+                on={state.followerCount.greaterThan}
+              />
+            </FormControl>
 
-        <FormControl header="Repo Count" label={repoLabel}>
-          <Switch
-            onClick={e => onChange(e, 'SET_REPO_COUNT')}
-            on={repoCount.greaterThan}
-          />
-        </FormControl>
-      </form>
+            <FormControl
+              header="Repo Count"
+              label={{ ...repoLabel, bool: state.repoCount.greaterThan }}
+            >
+              <Switch
+                onClick={e => callbacks.filterChangeHandler(e, 'SET_REPO_COUNT')}
+                on={state.repoCount.greaterThan}
+              />
+            </FormControl>
+          </form>
+        )}
+      </AppContext.Consumer>
     );
   }
 }
-
-SideBarForm.propTypes = {
-  language: PropTypes.string.isRequired,
-  limit: PropTypes.number.isRequired,
-  onChange: PropTypes.func.isRequired,
-  followerCount: PropTypes.objectOf(PropTypes.any).isRequired,
-  repoCount: PropTypes.objectOf(PropTypes.any).isRequired,
-  country: PropTypes.string,
-};
-
-SideBarForm.defaultProps = {
-  country: null,
-};
 
 export default SideBarForm;
